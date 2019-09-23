8096bfccc7c5e9890a7d98cd927d6baaf5680e81
jiesen <jieqqq678@163.com>
2019-09-24 07:13:09 +0800

    1

--- manager/src/router/index.js
+++ manager/src/router/index.js
@@ -29,16 +29,18 @@ router.beforeEach((to, from, next) => {
         });
     } else if (!token && to.name === 'login') {
         next();
-    } else if (token && to.name !== 'login') {
+    } else if (token && to.name === 'login') {
         next({
             name: 'home_index'
         });
     } else {
         const curRouterObj = util.getRouterObjByName([otherRouter, ...appRouter], to.name);
         if (curRouterObj && curRouterObj.title) {
+            console.log('111');
             util.title(curRouterObj.title, router.app);
         }
         if (curRouterObj && curRouterObj.access !== undefined) { // 需要判断权限的路由
+            console.log('222');
             if (curRouterObj.access === parseInt(Cookies.get('access'))) {
                 util.toDefaultPage([otherRouter, ...appRouter], to.name, router, next); // 如果在地址栏输入的是一级菜单则默认打开其第一个二级菜单的页面
             } else {
@@ -48,6 +50,7 @@ router.beforeEach((to, from, next) => {
                 });
             }
         } else { // 没有配置权限的路由, 直接通过
+            console.log('333');
             util.toDefaultPage([...routers], to.name, router, next);
         }
     }
